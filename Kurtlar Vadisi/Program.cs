3b01a21f6f84fe2b02d1c0aad0e34247f477e4ed
mirmezahir <mirmazahirmi-bp215@code.edu.az>
2024-10-18 05:06:29 -0700

    Weapon

---
+++ Kurtlar Vadisi/Program.cs
@@ -0,0 +1,103 @@
+﻿using System.Data;
+
+namespace Kurtlar_Vadisi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon weapon= new Weapon("AK-47",30,30,"Auto");
+            bool f = false;
+            bool g = false;
+            bool h = false;
+            do
+            {
+                Console.WriteLine("                                             KURTLAR VADISI            ");
+                Console.WriteLine("1.WEAPON INFORMATION\n2.SHOOT\n3.AMMO COUNT\n4.Reload\n5.Change Shoot mode\n6.ELave\n0.EXIT");
+                int login;
+               do
+                {
+                    Console.WriteLine("Reqem daxil edin !");
+                    g=int.TryParse(Console.ReadLine(), out login);
+                    
+                
+                }while (!g);
+                switch (login)
+                {
+                    case 1:
+                        
+                        weapon.Information();
+                        
+                        break;
+                      
+                    case 2:
+                        if (weapon.AmmoCount > 0) { weapon.Shoot(weapon.Mode); }
+                        else Console.WriteLine("NEED AMMO");
+                        break;
+
+                    case 3: weapon.GetRemainBulletCount(); break;
+                    case 4:
+                        weapon.Reload();
+                        break;
+                    case 5:
+                        weapon.ChangeFireMode();
+                        break;
+                    case 6:
+                        Console.Clear();
+                        
+                        do
+                        {
+                            Console.WriteLine("1.Tutumu artir\n2.Gulleni Artir\n3.Menyuya qayit ");
+                            do
+                            {
+                                bool p;
+                                int o;
+                                do
+                                {      
+                                     Console.WriteLine("Reqem daxil edin ");
+                                     p = int.TryParse(Console.ReadLine(), out o);
+                                } while (!p);
+                                switch (o) 
+                                
+                                { 
+                                    case 1:weapon.AmmoCapasity = weapon.AmmoCapasity + 10;
+                                        Console.WriteLine("Gulle tutumu artdi");
+
+                                    break;
+                                        case 2:
+                                        if (weapon.AmmoCount + 10 <= weapon.AmmoCapasity)
+                                        {
+                                            weapon.AmmoCount = weapon.AmmoCount + 10;
+                                        }
+                                        else Console.WriteLine("Gulle tutumu kecir");
+                                        break;
+                                        case 3:
+                                        h= true;
+                                        g= true;
+                                        break;
+
+
+
+
+                                }
+
+                            } while (!g);
+                            
+
+
+
+
+
+
+
+
+                        } while (!h);
+                        break;
+                    case 0: f = true; break;
+
+                }
+            } while (!f);
+            
+        }
+    }
+}
---
+++ Kurtlar Vadisi/Weapon.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kurtlar_Vadisi
+{
+    internal class Weapon
+    {
+        public string Name;
+        public int AmmoCapasity;
+        public int AmmoCount;
+        public bool Mode;
+       
+
+        public Weapon(string name, int ammoCapasity, int ammoCount,string mode)
+        {
+            Name = name;
+            AmmoCount = ammoCount;
+            AmmoCapasity = ammoCapasity;
+            
+            Mode = true; 
+        }
+
+        public void Shoot(bool mode )
+        {
+            Console.WriteLine("ATIS EDILDI ----------------------------------}");
+            if (AmmoCount <= AmmoCapasity&&AmmoCount>0)
+            {
+                if (mode == true)
+                {
+                    AmmoCount = 0; ;
+                }
+                else if (mode == false )
+                {
+                    AmmoCount--;
+                }
+            }
+           
+
+        }
+    
+
+        public void GetRemainBulletCount() 
+        { Console.WriteLine($"Qalan gulleniz ={AmmoCount}"); }
+        public void Reload() { AmmoCount = AmmoCapasity;Console.WriteLine("[Reloaded]"); }
+        public void ChangeFireMode() { if (Mode == true) { Mode = false ;Console.WriteLine("Simple mode kecdi"); } else if (Mode == false) { Mode = true;Console.WriteLine("Avtomatik mode kecdi  "); } }
+        
+        public void Information() { Console.WriteLine($"Weapon name :{Name} Ammo Capasity :{AmmoCapasity} Ammo:{AmmoCount}"); }
+    }
+}
